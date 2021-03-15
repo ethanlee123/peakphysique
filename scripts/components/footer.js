7b6be2d0bd27dcfec7dcca35e9f6243e8a136b93
Donna Dominguez Gamboa <dm.dominguezgamboa@gmail.com>
2021-03-15 13:41:36 -0700

    refactor footer comp js

--- scripts/components/footer.js
+++ scripts/components/footer.js
@@ -4,7 +4,7 @@ const path = "../../common/footer.html";
 
 class Footer extends HTMLElement {
     connectedCallback() {
-        const response = getTemplate(path)
+        getTemplate(path)
             .then((response) => {
                 const node = document.importNode(response.content, true);
                 document.body.appendChild(node);
