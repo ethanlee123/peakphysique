05ec77a20363347f68979a6b1524840efb3a2189
Ethan Lee <33e.lee@gmail.com>
2021-03-17 14:29:31 -0700

    re-structured template for firestore data structure

--- scripts/schema.js
+++ scripts/schema.js
@@ -1,90 +1,154 @@
-const trainer = {
-    firstName: "firstName1",
-    lastName: "lastName1",
-    location: new firebase.firestore.Geopoint(37.422, 122.084),
-    gender: "Male",
-    age: "28",
-    profilePic: "./profile-pic.jpg",
-    website: "trainermike.com",
-    services: [
-        "test1",
-        "test2",
-        "test3"
-    ],
-    expertise: [
-        "bodybuilding",
-        "endurance training",
-        "weight loss"
-    ],
-    hourlyRate: 40,
-    firstSessionFree: true,
-    appointments: 
-    [
-        {
-            date: new firebase.firestore.Timestamp.now(),
-            time: "Morning",
-            available: true
-        },
-        {
-            date: new firebase.firestore.Timestamp.fromDate(new Date("March 16 2021")),
-            time: "Afternoon",
-            available: false
-        }
-    ],
-    yearsOfExperience: 3,
-    totalClients: 5,
-    totalSessions: 10,
+// will use this for editing trainer profile
+var trainer = {
+    age: -1, // integer
+    email: "",
+    firstSessionFree: null, // boolean
+    gender: "", // String male, female, other, or prefer not to say
+    hourlyRate: 0, // integer
+    location: new firebase.firestore.Geopoint(37.422, 122.084), // Retrieved automatically
+     // String, link to website
+    rating: -1, // integer, range 1 - 5
+    totalClients: -1, // integer
+    totalSessions: -1, // integer
+    profilePic: "./profile-pic.jpg", // String path to file?
+    expertise: {
+        certification: "path to file?",
+        yearsOfExperience: 0, // integer
+        fitness: [
+            "body building",
+            "power lifting"
+        ],
+        wellness: [
+            "macro coaching",
+            "therapy"
+        ]
+    },
+    name: {
+        first: "",
+        last: ""
+    },
+    platformSpecific: {
+        generalAvailability: ["sun", "mon", "tue", "wed", "thu", "fri", "sat"],
+        time: ["morning", "afternoon", "evening"]
+    },
     posts: [
         {
             date: new firebase.firestore.Timestamp.now(),
             message: "Hello world!"
         }
     ],
-    certifications: [
-        "blah",
-        "yadda",
-        "toodles"
-    ],
     socialMedia: {
-        facebook: "facebook.com/trainermike",
-        instagram: "instagram.com/trainermike"
-    }
+        facebook: "facebook.com/trainermike", // String, link to social media
+        instagram: "instagram.com/trainermike", // String, link to social media
+        website: "https://trainermike.com"
+    },
 }
 
-const user = {
-    firstName: "firstName2",
-    lastName: "lastName2",
-    email: "user@email.com",
-    role: "Client", // or trainer or null, if they haven't set it yet
-    profilePic: "",
-    gender: "Male",
-    age: 25,
-    location: "37.422, 122.084",
+// User includes both trainer and client
+var user = {
+    age: -1, // integer
+    email: "",
+    name: {
+        first: "",
+        last: ""
+    },
     fitnessGoals: [
-        "Curl 100 lbs",
-        "Bench 2 plates",
-        "Squat 350 lbs"
+        "", 
+        "",
+        ""
     ],
-    fitnessLevel: "Beginner",
-    favWorkout: "Running",
-    favMeal: "Pizza", 
+    favCheatMeal: "",
+    favWorkout: "",
+    fitnessLevel: "",
+    gender: "", // String male or female or other or prefer not to say
+    location: new firebase.firestore.Geopoint(37.422, 122.084), // Retrieved automatically
+    profilePic: "", //uploaded by the user
+    rating: -1, // integer, range 1 - 5
+    role: "" // String, client or trainer or null, if they haven't set it yet
+};
 
-    rating: "5" // range 1 - 5
+// use this for seeing user profiles
+var userProfile = {
+    name: {
+        first: "",
+        last: ""
+    },
+    city: "", //extract from geolocation
+    totalClients: 0, 
+    totalSessions: 0,
+    about: {
+        favCheatMeal: "",
+        favWorkout: "",
+        fitnessLevel: "",
+        fitnessGoal: "",
+        website: ""
+    },
+    updates: {
+        title: "",
+        date: new firebase.firestore.Timestamp.now(),
+        content: ""
+    },
+    hourlyRate: 0,
+    fitness: [
+        "",
+        ""
+    ],
+    wellness: [
+        "",
+        ""
+    ],
+    certifications: [
+        "",
+        ""
+    ]
 }
 
+
+// use this for my schedule page
+var appointments = {
+    date: new firebase.firestore.Timestamp.fromDate(new Date("March 16 2021")), // Timestamp
+    location: "",
+    completed: false, // boolean default false
+    sessionTitle: "",
+    clientProfilePic: "",
+    clientFirstName: "",
+    clientLasttName: "",
+    trainerProfilePic: "",
+    trainerFirstName: "",
+    trainerLasttName: "",
+    confirmationId: "",
+
+    appointments: 
+    [
+        {
+            date: new firebase.firestore.Timestamp.now(), // returns Timestamp object
+            time: "", // String, morning or afternoon or evening
+            available: true // boolean
+        },
+        {
+            date: new firebase.firestore.Timestamp.fromDate(new Date("March 16 2021")),
+            time: "", // String, morning or afternoon or evening
+            available: false   // boolean
+        }
+    ],
+}
+
+
+// use subcollection or root level collections structured data
 const conversations = [
     {
-        users: [
-            "id1",
-            "id2"
-        ],
         messages: [
             {
-                fromUser: "id1",
+                fromUser: "id1", 
+                toUser: "id2",
                 date: "Timestamp",
                 message: "Blahbbity blah",
                 seen: false // how do ???
             }
-        ]
+        ],
+        users: [
+            "id1",
+            "id2"
+        ],
     }
 ];
