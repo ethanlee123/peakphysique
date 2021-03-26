f92ec4b224d9b735620293f154bd314a05ba19e2
Andy Chang <ychang118@my.bcit.ca>
2021-03-25 21:38:49 -0700

    added confirm button js to write to schedule collection for booking appointments

--- scripts/features/book-appointment-schedule.js
+++ scripts/features/book-appointment-schedule.js
@@ -1,3 +1,14 @@
-import { displayScheduleInfo } from "/scripts/api/firebase-queries.js";
+import { displayScheduleInfo, writeAppointmentSchedule } from "/scripts/api/firebase-queries.js";
+
+// initial message from client
+const comments = document.getElementById("comments");
+const timeSlot = document.getElementById("time-slot");
+const date = document.getElementById("datepicker");
+const confirmBtn = document.getElementById("confirm-btn")
+
+confirmBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    writeAppointmentSchedule(comments, timeSlot, date);
+})
 
 // displayScheduleInfo();
