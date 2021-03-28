07109c056f3998b699f437a4d328c5880c85f88c
Donna Dominguez Gamboa <dm.dominguezgamboa@gmail.com>
2021-03-27 18:33:31 -0700

    add 404 redirect to user prof

--- scripts/features/user-profile.js
+++ scripts/features/user-profile.js
@@ -2,9 +2,9 @@ import { displayAboutMe } from "../api/firebase-queries.js";
 import { displayTrainerInfo, hideUserSections, trainerProfilePosts } from "/scripts/api/firebase-queries.js";
 
 let trainerToDisplay = localStorage.getItem("trainerProfileToDisplay");
-trainerToDisplay = JSON.parse(trainerToDisplay);
-localStorage.setItem("trainerProfileToDisplay", "");
+trainerToDisplay = trainerToDisplay ? JSON.parse(trainerToDisplay) : window.location.href="./404.html";
 console.log(trainerToDisplay);
+localStorage.setItem("trainerProfileToDisplay", "");
 // displayTrainerInfo();
 // hideUserSections();
 // trainerProfilePosts();
