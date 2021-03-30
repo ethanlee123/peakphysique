a45de21a75939b7b39b4603fd068991b7eff0b73
Donna Dominguez Gamboa <dm.dominguezgamboa@gmail.com>
2021-03-30 10:48:22 -0700

    add header page indicator for desktop

--- scripts/components/header.js
+++ scripts/components/header.js
@@ -2,6 +2,12 @@ import { getTemplate } from "../util/getTemplate.js";
 
 const path = "../../common/header.html";
 var isLoggedIn = false;
+const routes = [
+    "index",
+    "schedule",
+    "find-a-trainer",
+    "group-class"
+];
 
 class Header extends HTMLElement {
     constructor() {
@@ -13,16 +19,30 @@ class Header extends HTMLElement {
             .then((response) => {
                 const node = document.importNode(response.content, true);
                 document.body.prepend(node);
-                const burgerMenu = document.body.querySelector(".burger-menu");
-                burgerMenu.addEventListener("click", () => {
-                    document.body.classList.contains("active-nav") ? document.body.classList.remove("active-nav") : document.body.classList.add("active-nav");
-                });
+                handleBurgerMenu();
+                showCurrentPage();
             });
 
     }
 }
 
 customElements.define("header-component", Header);
+
+const handleBurgerMenu = () => {
+    const burgerMenu = document.body.querySelector(".burger-menu");
+    burgerMenu.addEventListener("click", () => {
+        document.body.classList.contains("active-nav") ? document.body.classList.remove("active-nav") : document.body.classList.add("active-nav");
+    });
+}
+
+const showCurrentPage = () => {
+    const navLinks = document.querySelectorAll("#navLinks a.nav-link");
+    navLinks.forEach(link => {
+        if (window.location.pathname === link.getAttribute("href")) {
+            link.classList.add("current-page");
+        }
+    });
+}
 
 window.addEventListener("resize", () => {
     document.body.classList.remove("active-nav");
