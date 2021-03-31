6c349d5405097fb142cf0f36a680105224493158
Josef Murillo <jdmurillo93@gmail.com>
2021-03-30 19:54:14 -0700

    small update to user profile updates

--- scripts/features/user-profile.js
+++ scripts/features/user-profile.js
@@ -1,16 +1,36 @@
 import { displayAboutMe } from "../api/firebase-queries.js";
-import { displayTrainerInfo, hideUserSections, trainerProfilePosts } from "/scripts/api/firebase-queries.js";
+import { displayTrainerInfo, hideUserSections } from "/scripts/api/firebase-queries.js";
 
 let trainerToDisplay = localStorage.getItem("trainerProfileToDisplay");
 trainerToDisplay = trainerToDisplay ? JSON.parse(trainerToDisplay) : window.location.href="./404.html";
-console.log(trainerToDisplay);
+const trainerID = trainerToDisplay.userId
 localStorage.setItem("trainerProfileToDisplay", "")
+
+const db = firebase.firestore();
 
 
 
 // displayTrainerInfo();
 // hideUserSections();
-// trainerProfilePosts();
+function trainerProfilePosts() {
+    console.log(trainerID);
+
+    db.collection("trainerOnly").doc(trainerID).get()         // pulls from "Chuck Norris" doc
+    .then(function(doc){
+            let postDate = doc.data().posts.Date.toDate().toDateString();
+            console.log(postDate);
+            let postMsg = doc.data().posts.message;
+            console.log(postMsg);
+            let postTitle= doc.data().posts.title;
+            console.log(postTitle);
+
+            document.getElementById('postDate').innerText = postDate;
+            document.getElementById('postMessage').innerText = postMsg;
+            document.getElementById('postTitle').innerText = postTitle;
+        })
+    }
+
+trainerProfilePosts();
 // displayAboutMe();
 
 
