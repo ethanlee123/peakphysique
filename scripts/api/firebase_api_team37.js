008ccc358c54cb87f0ec1431be3c0fb94b4ccbeb
Donna Dominguez Gamboa <dm.dominguezgamboa@gmail.com>
2021-04-01 16:25:00 -0700

    delete signInSuccess callback

--- scripts/api/firebase_api_team37.js
+++ scripts/api/firebase_api_team37.js
@@ -17,14 +17,7 @@ function firebaseUI() {
   var uiConfig = {
     callbacks: {
       signInSuccessWithAuthResult: async (authResult, redirectUrl) => {
-        localStorage.getItem("user") && localStorage.removeItem("user");
-        const { user } = authResult;
-        const res = await getUser(user.uid);
-        if (res) {
-          localStorage.setItem("user", JSON.stringify(res));
-          window.location.href = "../../sign-up-success.html";
-        }
-
+        return true;
       },
       uiShown: function () {
         document.getElementById('loader').style.display = 'none';
